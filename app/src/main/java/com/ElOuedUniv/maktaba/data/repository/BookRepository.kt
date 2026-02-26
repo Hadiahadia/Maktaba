a4aae3a4c33cb021f6cad309f07adb4f748e4eca
hadia <mehayechhadia@gmail.com>
2026-02-25 16:07:23 -0800

    TP1: Complete book information and add 5 new books

--- app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
@@ -23,12 +23,24 @@ class BookRepository {
      * )
      */
     private val booksList = listOf(
-        Book(isbn = "", title = "Clean Code", nbPages = 0),
-        Book(isbn = "", title = "The Pragmatic Programmer", nbPages = 0),
-        Book(isbn = "", title = "Design Patterns", nbPages = 0),
-        Book(isbn = "", title = "Refactoring", nbPages = 0),
-        Book(isbn = "", title = "Head First Design Patterns", nbPages = 0)
+        Book(isbn = "978-0-13-235088-4", title = "Clean Code", nbPages = 464),
+        Book(isbn = "978-0-13-595705-9", title = "The Pragmatic Programmer", nbPages = 352),
+        Book(isbn = "978-0-20-163361-0", title = "Design Patterns", nbPages = 395),
+        Book(isbn = "978-0-13-475759-9", title = "Refactoring", nbPages = 448),
+        Book(isbn = "978-59-600712-6", title =  "Perl Cookbook", nbPages = 791),
+        Book(isbn = "978-15-6592243-3", title = "How to Design Programes", nbPages = 791),
+        Book(isbn = "978-05-9652684-9", title = "Head First SQL", nbPages = 500),
+        Book(isbn = "978-14-9207652-0", title = "Head First Android Development", nbPages = 930),
+        Book(isbn = "978-14-9195432-4", title = "Learning Perl", nbPages = 394),
+        Book(isbn = "978-02-0170431-1", title = "Modren C++ Design", nbPages = 323)
     )
+
+
+
+
+
+
+
 
     /**
      * TODO for Students (TP1 - Exercise 2):
